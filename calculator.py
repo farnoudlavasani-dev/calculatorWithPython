b278ce5a92c88a1c7b9d531dc4a09333428ccd42
farnoudlavasani-dev <lavasanifarnoud@gmail.com>
2024-08-21 09:53:55 +0330

    fix the bugs :D

--- calculator.py
+++ calculator.py
@@ -1,51 +1,44 @@
 
 def add(a, b):
-    result = a + b
-    return result
+    return a + b
 def differentiation(a, b):
-    result = a - b
-    return result
+    return a - b
 def multiplication(a, b):
-    result = a * b
-    return result
+    return a * b
 def division(a, b):
-    result = a / b
-    return result
+    if b == 0:
+        return "Error: Division by zero is not allowed in my world"
+    return a / b
 def Exponentiation(a, b):
-    result = a ** b
-    return result
-while True:
-    print("hello world. this is a simple Calculator")
-    print("1.Add")
-    print("2.Subtract")
-    print("3.Multiply")
-    print("4.Divide")
-    print("5.Exponentiation")
-    print("6.Exit")
+    return a ** b
+print("hello. this is a simple Calculator")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Exponentiation")
+print("6.Exit")
+
+while True: 
     choice = input("Select operation | Enter choice(1/2/3/4/5/6):")
     if choice == "6":
         print("goodbye")
         break
-    else:
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
+    elif choice in ["1", "2", "3", "4", "5"]:
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
         if choice == "1":
-            add(num1, num2)
+            print(add(num1, num2))
         elif choice == "2":
-            differentiation(num1, num2)
+            print(differentiation(num1, num2))
         elif choice == "3":
-            multiplication(num1 , num2)
+            print(multiplication(num1, num2))
         elif choice == "4":
-            division(num1, num2)
+            print(division(num1, num2))
         elif choice == "5":
-            Exponentiation(num1, num2)
-        else:
-            print("Invalid input")
-
-
-    
-
+            print(Exponentiation(num1, num2))
+    else:
+        print("Invalid input")
         
 
 
